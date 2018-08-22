06301688c041b6962066327c61b1f0eb276328dd
Jake Levi <jakelevi1996@gmail.com>
2018-08-22 14:14:52 +0100

    Removed unused file

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Launch tensorboard in background to keep tabs
-(tensorboard --logdir log &) &> /dev/null
-
-# Launch synthetic data training. For testing
-python3 train_agent.py --data syn --print_every 1 --num_epochs 20 --headless True
-
